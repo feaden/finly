3f117ce208538df8ecc0ccdcf8b1528df254bf11
William <test@test.com>
2025-07-06 17:58:50 +0300

    git init

---
+++ src/test/java/com/app/finly/FinlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.finly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
